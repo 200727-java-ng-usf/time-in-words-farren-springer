d9c659cfdcd490893aea4c0466bf185f94c1d1ce
Farren Springer <68714743+fjs507@users.noreply.github.com>
2020-10-23 14:23:15 -0500

    mock data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,3 @@
-INSERT INTO APP_USER (NAME) VALUES ('farren')
-INSERT INTO APP_USER (NAME) VALUES ('farren2')
-INSERT INTO APP_USER (NAME) VALUES ('farren3')
-INSERT INTO APP_USER (NAME) VALUES ('farren4')
+INSERT INTO EVENT (NAME, LOCATION, DATE, H, M) VALUES ('farren goes to work', 'Oklahoma','10/23/20', 9, 01)
+INSERT INTO EVENT (NAME, LOCATION, DATE, H, M) VALUES ('farren gets off work', 'Oklahoma','10/23/20', 5, 01)
+INSERT INTO EVENT (NAME, LOCATION, DATE, H, M) VALUES ('farren goes to sleep', 'Oklahoma','10/23/20', 10, 01)
